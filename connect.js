3775f0ba0ca321b0b1b3aef527ab19b49fb6e9a3
TuNA <nguyentu2392@gmail.com>
2025-03-19 17:04:56 +0700

    feat/Authentication: register, login, logout

--- connect.js
+++ connect.js
@@ -1,8 +1,25 @@
-import mysql from "mysql";
+// import mysql from "mysql";
+
+// export const db = mysql.createConnection({
+//     host:"localhost",
+//     user:"root",
+//     password:"",
+//     database:"social"
+// })
+import mysql from "mysql2"
 
 export const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"social"
-})
+  host: "localhost",
+  user: "root",
+  port:"3306",
+  password: "123456",
+  database: "social"
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Connection error:', err);
+  } else {
+    console.log('Connected to MySQL!');
+  }
+});
--- index.js
+++ index.js
@@ -4,11 +4,14 @@ import postRoutes from "./routes/posts.js";
 import likeRoutes from "./routes/likes.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
-
+import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
 
 //middlewares
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
