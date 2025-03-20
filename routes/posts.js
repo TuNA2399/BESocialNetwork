4d169bd628e98e66fa6c5cb3d8067a22290377d3
TuNA <nguyentu2392@gmail.com>
2025-03-20 22:23:19 +0700

    feat/Post: get, add

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,8 @@
 import express from "express";
-import {  } from "../controller/post.js";
+import {getPosts, addPost} from "../controller/post.js";
 const router =  express.Router();
 
-router.get("", )
+router.get("/", getPosts);
+router.post("/", addPost);
 
 export default router;
