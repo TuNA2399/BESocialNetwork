7df9ebe738dd1945b1229e8cb434992434bde06e
TuNA <nguyentu2392@gmail.com>
2025-03-21 16:33:34 +0700

    feat/Post: update add post with image using multer

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ import likeRoutes from "./routes/likes.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 import cors from "cors";
+import multer from "multer";
 import cookieParser from "cookie-parser";
+
 const app = express();
 
 //middlewares
@@ -19,6 +21,21 @@ app.use(cors({
 }
 ));
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '../FE/public/upload')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname);
+    }
+})
+
+const upload = multer({ storage: storage })
+app.post("/api/upload", upload.single("file"), (req, res)=>{
+    const file = req.file;
+    res.status(200).json(file.filename);
+})
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
