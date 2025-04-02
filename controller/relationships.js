0041a309532b4889387304bb3ca48552b1801aac
TuNA <nguyentu2392@gmail.com>
2025-04-02 19:16:26 +0700

    get unfollowed User

--- controller/relationships.js
+++ controller/relationships.js
@@ -31,6 +31,26 @@ export const getFriends = (req, res) => {
 
 }
 
+export const getUnfollowed = (req, res) => {
+    const token = req.cookies.accessToken;
+    jwt.verify(token, "secretkey", (err, userInfo) => {
+        if (err) return res.status(403).json("Token is invalid!!");
+
+        const q = `SELECT u.id, u.name, u.profilePic 
+                    FROM users AS u
+                    LEFT JOIN relationships AS r 
+                        ON u.id = r.followedUserId 
+                        AND r.followerUserId = ?
+                    WHERE r.followedUserId IS NULL 
+                      AND u.id <> ?`;
+
+        db.query(q, [userInfo.id, userInfo.id], (err, data) => {
+            if (err) return res.status(500).json(err);
+            return res.status(200).json(data);
+        });
+    });
+};
+
 export const addRelationship = (req, res) => {
     const token = req.cookies.accessToken;
     if (!token) return res.status(401).json("Not logged in!!");
--- routes/relationships.js
+++ routes/relationships.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getRelationships, addRelationship, deleteRelationship, getFriends} from "../controller/relationships.js";
+import { getRelationships, addRelationship, deleteRelationship, getFriends, getUnfollowed} from "../controller/relationships.js";
 const router =  express.Router();
 
 router.get("/", getRelationships)
 router.get("/friends", getFriends)
+router.get("/unfollowed", getUnfollowed)
 router.post("/", addRelationship)
 router.delete("/", deleteRelationship)
 
