6920affa5f2632672484342af6824bb3e117c476
TuNA <nguyentu2392@gmail.com>
2025-04-12 20:46:41 +0700

    update jst

--- controller/auth.js
+++ controller/auth.js
@@ -22,8 +22,6 @@ export const register = (req, res) => {
         })
     })
 
-
-
 }
 
 export const login = (req, res) => {
--- index.js
+++ index.js
@@ -54,6 +54,22 @@ app.use("/api/relationships", relationshipRoutes);
 app.use("/api/stories", strRoutes);
 app.get("/api/signature", generateCloudinarySignature);
 
+//Refresh token
+app.post("/refresh-token", (req, res) => {
+    const refreshToken = req.cookies.refreshToken;
+  
+    if (!refreshToken) return res.status(401).json("No refresh token");
+  
+    jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, (err, user) => {
+      if (err) return res.status(403).json("Invalid refresh token");
+  
+      const newAccessToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: "5h" });
+  
+      res.json({ accessToken: newAccessToken });
+    });
+  });
+  
+
 app.listen(8800, () => {
     console.log("API running !!");
 });
