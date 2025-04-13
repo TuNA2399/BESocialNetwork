8e826834c0d17b4b87ec20e41248e43d50b44c10
TuNA <nguyentu2392@gmail.com>
2025-04-13 20:30:17 +0700

    fix/auth: update jwt and add refresh token

--- controller/auth.js
+++ controller/auth.js
@@ -1,51 +1,68 @@
-import { db } from "../connect.js"
+import { db } from "../connect.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
-    //Check user if exist
-    const q = "SELECT * FROM users WHERE username = ?"
+    const q = "SELECT * FROM users WHERE username = ?";
+
     db.query(q, [req.body.username], (err, data) => {
         if (err) return res.status(500).json(err);
-        if (data.length) return res.status(409).json("User already exits!!")
-        //Create a new user
-        //Hash the password
+        if (data.length) return res.status(409).json("User already exists!!");
+
         const salt = bcrypt.genSaltSync(10);
         const hashPassword = bcrypt.hashSync(req.body.password, salt);
 
-        const q = "INSERT INTO users (`username`, `email`, `password`, `name`) VALUE (?)"
+        const insertQuery = "INSERT INTO users (`username`, `email`, `password`, `name`) VALUES (?)";
+        const values = [req.body.username, req.body.email, hashPassword, req.body.name];
 
-        const values = [req.body.username, req.body.email, hashPassword, req.body.name]
-        db.query(q, [values], (err, data) => {
+        db.query(insertQuery, [values], (err, data) => {
             if (err) return res.status(500).json(err);
             return res.status(200).json("User has been created!");
-        })
-    })
-
-}
+        });
+    });
+};
 
 export const login = (req, res) => {
-    const q = "SELECT * FROM users WHERE username = ?"
+    const q = "SELECT * FROM users WHERE username = ?";
+
     db.query(q, [req.body.username], (err, data) => {
         if (err) return res.status(500).json(err);
-        if(data.length === 0) return res.status(404).json("User not found!!");
+        if (data.length === 0) return res.status(404).json("User not found!!");
 
         const checkPassword = bcrypt.compareSync(req.body.password, data[0].password);
-        if(!checkPassword) return res.status(400).json("Wrong password or username");
-        
-        const token = jwt.sign({id:data[0].id}, "secretkey");
-        
-        const {password, ...others} = data[0];
+        if (!checkPassword) return res.status(400).json("Wrong password or username");
 
-        res.cookie("accessToken", token, {
+        const accessToken = jwt.sign(
+            { id: data[0].id },
+            "secretkey",
+            { expiresIn: "5h" }
+        );
+
+        const refreshToken = jwt.sign(
+            { id: data[0].id },
+            "refreshSecretKey",
+            { expiresIn: "30d" }
+        );
+
+        res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
-        }).status(200).json(others);
-    })
-}
+            secure: true,
+            sameSite: "strict",
+            maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+        });
+
+        const { password, ...userInfo } = data[0];
+
+        return res.status(200).json({
+            accessToken,
+            user: userInfo,
+        });
+    });
+};
 
 export const logout = (req, res) => {
-    res.clearCookie("accessToken",{
+    res.clearCookie("refreshToken", {
         secure: true,
-        sameSite:"none"
-    }).status(200).json("User has been logged out!!")
-}
+        sameSite: "strict",
+    }).status(200).json("User has been logged out!!");
+};
